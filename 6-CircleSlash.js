8320e1d9d93ff1fc4a76942bf39ad3c13b159ab0
onarman <onarmannn@gmail.com>
2023-04-07 16:42:06 +0300

    changed

--- 6-CircleSlash.js
+++ 6-CircleSlash.js
@@ -21,10 +21,9 @@
 // The index of the last person standing. 
 
 function circleSlash(n) {
-    let index = 0;
-    for (let i = 1; i <= n; i++) {
-      index = (index + 2) % i;
-    }
-    return index + 1;
+    return n === 1 ? 1 :
+      n % 2 ?
+        2 * circleSlash((n - 1) / 2) + 1
+      : 2 * circleSlash(n / 2) - 1
   }
 
